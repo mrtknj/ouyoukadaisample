d40295a696693f9a35210d46aa51ab4a6df11153
mrtknj <105144477+mrtknj@users.noreply.github.com>
2022-07-08 16:43:06 +0000

    応用課題７８９a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :user_rooms 
+  has_many :chats
+  has_many :rooms, through: :user_rooms
+  
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_books, through: :favorites, source: :book
+  
+  has_many :view_counts, dependent: :destroy
   
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
